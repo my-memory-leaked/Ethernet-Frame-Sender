c9ac5de83bf0741aad6577791a2a44e801ad639b
Karol <pisek.x@gmail.com>
2022-10-04 12:18:34 +0200

    Added comments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import socket
 import time
 
+# WORKS UDP ONLY NOW !!!
 CONFIG_FILE_PATH = "config.txt"
 MESSAGE_FILE_PATH = "message.txt"
 
@@ -10,7 +11,7 @@ CONFIG_FILE = open(CONFIG_FILE_PATH, mode ="r",
 MESSAGE_FILE = open(MESSAGE_FILE_PATH, mode ="r",
                     encoding = "utf-8")
 
-
+# Reads config from config.txt file
 UDP_IP = CONFIG_FILE.readline().rstrip("\n")
 UDP_PORT = int( CONFIG_FILE.readline() )
 SERVER_ADDRESS = (UDP_IP , UDP_PORT)
@@ -19,19 +20,21 @@ INTERNET_SOCKET = socket.socket( socket.AF_INET,     # Internet
                                  socket.SOCK_DGRAM ) # UDP
 
 DELAY = int( CONFIG_FILE.readline() )
-MS_DELAY = DELAY/1000
+MS_DELAY = DELAY/1000 # Delay time between sending Ethernet Frames
 
 print("UDP target IP: %s" % UDP_IP)
 print("UDP target port: %s" % UDP_PORT)
 print("Sending frame delay in ms: %s" % MS_DELAY)
 
-FRAMES = []
+FRAMES = [] # Declaration of list to contain messages
 
+# Adding messages to the list
 for LINE in MESSAGE_FILE:
     ELEMENT = LINE[:-1]
     FRAMES.append( ELEMENT )
 
-
+# Sending frames via UDP
+# WARNING infinite loop
 while True:
     for FRAME in FRAMES:
         print(FRAME)
@@ -40,5 +43,6 @@ while True:
         time.sleep(MS_DELAY)
     print("Sending again!!!")
 
+# Closing files
 CONFIG_FILE.close()
 MESSAGE_FILE.close()
